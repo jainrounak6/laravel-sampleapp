1dfce17cd28ad7160d6ea5677ee6be2b3bb56808
Rounak Jain <35358530+jainrounak6@users.noreply.github.com>
2020-09-26 16:12:24 +0530

    mysql user setup

--- start.sh
+++ start.sh
@@ -5,6 +5,11 @@
 #sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
 
 # Start supervisord and services
+MYSQL_PASSWORD=test123
+MYSQL_USER=testuser
+MYSQL_DATABASE=testdb
+
 docker-entrypoint.sh
 service mysql start
+mysql -e "CREATE DATABASE $MYSQL_DATABASE;CREATE USER $MYSQL_USER@localhost identified by '$MYSQL_PASSWORD';GRANT ALL ON $MYSQL_DATABASE.* to $MYSQL_USER@localhost WITH GRANT OPTION; FLUSH PRIVILEGES;"
 /usr/local/bin/supervisord -n -c /etc/supervisord.conf
