a0ce5d2b87b23bd0a59055e13e19ebfa42a77a1f
Rounak Jain <35358530+jainrounak6@users.noreply.github.com>
2020-09-26 13:31:01 +0530

    docker-entrypoint

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Update nginx to match worker_processes to no. of cpu's
-procs=$(cat /proc/cpuinfo | grep processor | wc -l)
-sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
+#procs=$(cat /proc/cpuinfo | grep processor | wc -l)
+#sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
 
 # Start supervisord and services
+docker-entrypoint.sh
+service mysql start
 /usr/local/bin/supervisord -n -c /etc/supervisord.conf
